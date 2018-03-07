ba3a1930333858d809f553acada22ec8e5ccc7dc
Jaewook Jung <jw0213.jung@samsung.com>
2018-03-07 16:24:51 +0900

    delete tmp files
    Signed-off-by: Jaewook Jung <jw0213.jung@samsung.com>

--- src/tmp/SupportedRoleClass.cpp
+++
@@ -1,9 +0,0 @@
-#include "SupportedRoleClass.h"
-
-std::string SupportedRoleClass::getRefRoleClassPath() {
-    return refRoleClassPath;
-}
-
-void SupportedRoleClass::setRefRoleClassPath(std::string refbaseroleclasspath) {
-    refRoleClassPath = refbaseroleclasspath;
-}
