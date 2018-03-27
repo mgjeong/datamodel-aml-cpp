f8630c4426d16ec125aadacd90b3bc2e5f5c2633
Jaewook Jung <jw0213.jung@samsung.com>
2018-03-27 19:28:58 +0900

    Fixed build script for arm building
    Signed-off-by: Jaewook Jung <jw0213.jung@samsung.com>

--- build_common.sh
+++ build_common.sh
@@ -29,11 +29,56 @@ AML_WITH_DEP=false
 AML_BUILD_MODE="release"
 AML_LOGGING="off"
 
+RELEASE="1"
+LOGGING="0"
+
 install_dependencies() {
+    # download required tool chain for cross compilation [arm/arm64/armhf]
+    if [ "arm" = ${AML_TARGET_ARCH} ]; then
+        if [ -x "/usr/bin/arm-linux-gnueabi-g++" ] && [ -x "/usr/bin/arm-linux-gnueabi-gcc" ]; then
+            echo -e "${BLUE}Cross compile tool-chain found for arm${NO_COLOUR}"
+        else
+            echo -e "${RED}No cross compile tool-chain found for arm, install using below commands:${NO_COLOUR}"
+            echo -e "${BLUE} $ sudo apt-get update"
+            echo -e " $ sudo apt-get install gcc-arm-linux-gnueabi"
+            echo -e " $ sudo apt-get install g++-arm-linux-gnueabi${NO_COLOUR}"
+            exit 0
+        fi
+    elif [ "arm64" = ${AML_TARGET_ARCH} ]; then
+        if [ -x "/usr/bin/aarch64-linux-gnu-g++-4.8" ] && [ -x "/usr/bin/aarch64-linux-gnu-gcc-4.8" ] && [ -x "/usr/bin/aarch64-linux-gnu-g++" ] && [ -x "/usr/bin/aarch64-linux-gnu-gcc" ]; then
+            echo -e "${BLUE}Cross compile tool-chain found for arm64${NO_COLOUR}"
+        else
+            echo -e "${RED}No cross compile tool-chain found for arm64, install using below commands:${NO_COLOUR}"
+            echo -e "${BLUE} $ sudo apt-get update"
+            echo -e " $ sudo apt-get install gcc-4.8-aarch64-linux-gnu"
+            echo -e " $ sudo apt-get install g++-4.8-aarch64-linux-gnu"
+            echo -e " $ cd /usr/bin"
+            echo -e " $ sudo ln -s aarch64-linux-gnu-g++-4.8 aarch64-linux-gnu-g++"
+            echo -e " $ sudo ln -s aarch64-linux-gnu-gcc-4.8 aarch64-linux-gnu-gcc${NO_COLOUR}"
+            exit 0
+        fi
+    elif [ "armhf" = ${AML_TARGET_ARCH} ]; then
+        if [ -x "/usr/bin/arm-linux-gnueabihf-g++-4.8" ] && [ -x "/usr/bin/arm-linux-gnueabihf-gcc-4.8" ] && [ -x "/usr/bin/arm-linux-gnueabihf-g++" ] && [ -x "/usr/bin/arm-linux-gnueabihf-gcc" ]; then
+            echo -e "${BLUE}Cross compile tool-chain found for armhf${NO_COLOUR}"
+        else
+            echo -e "${RED}No cross compile tool-chain found for armhf, install using below commands:${NO_COLOUR}"
+            echo -e "${BLUE} $ sudo apt-get update"
+            echo -e " $ sudo apt-get install gcc-4.8-arm-linux-gnueabihf"
+            echo -e " $ sudo apt-get install g++-4.8-arm-linux-gnueabihf"
+            echo -e " $ cd /usr/bin"
+            echo -e " $ sudo ln -s arm-linux-gnueabihf-g++-4.8 arm-linux-gnueabihf-g++"
+            echo -e " $ sudo ln -s arm-linux-gnueabihf-gcc-4.8 arm-linux-gnueabihf-gcc${NO_COLOUR}"
+            exit 0
+        fi
+    fi
+
+    # Check, clone, build and install the depencency libraries
     cd ./dependencies
     DEP_ROOT=$(pwd)
-    
+
+    # build, install protobuf library
     FILENAME="protobuf-cpp-3.4.0.tar.gz"
+    cd $DEP_ROOT
     if [ -e "$FILENAME" ]; then
         echo "Protobuf tar exist"
     else
@@ -57,6 +102,7 @@ install_dependencies() {
         echo -e "${BLUE}Protobuf configuring for arm64${NO_COLOUR}"
         ./configure --host=aarch64-unknown-linux-gnu CC=/usr/bin/aarch64-linux-gnu-gcc-4.8 CXX=/usr/bin/aarch64-linux-gnu-g++-4.8
         make -j 4
+        sudo make install
     elif [ "armhf" = ${AML_TARGET_ARCH} ]; then
         echo -e "${BLUE}Protobuf configuring for armhf${NO_COLOUR}"
         ./configure --host=arm-linux-gnueabihf CC=arm-linux-gnueabihf-gcc-4.8 CXX=arm-linux-gnueabihf-g++-4.8
@@ -65,6 +111,13 @@ install_dependencies() {
         ./configure
         make -j 4
         sudo make install
+    fi
+
+    #handle protobuf error for cross compilation
+    if [ "arm" = ${AML_TARGET_ARCH} ] || [ "arm64" = ${AML_TARGET_ARCH} ] || [ "armhf" = ${AML_TARGET_ARCH} ] || [ "armhf-qemu" = ${AML_TARGET_ARCH} ]; then
+        echo -e "${BLUE}Copying libs from protobuf built directory to /usr/local/lib${NO_COLOUR}"
+        sudo cp src/.libs/protoc /usr/bin
+        sudo cp src/.libs/* /usr/local/lib
     fi
     sudo ldconfig
     echo -e "${GREEN}Install dependencies done${NO_COLOUR}"
@@ -80,14 +133,56 @@ usage() {
     echo "  -h / --help                                                  :  Display help and exit"
 }
 
+
+
+build_x86() {
+    echo -e "Building for x86"
+    scons TARGET_OS=linux TARGET_ARCH=x86 RELEASE=${RELEASE} LOGGING=${LOGGING}
+}
+
+build_x86_64() {
+    echo -e "Building for x86_64"
+    scons TARGET_OS=linux TARGET_ARCH=x86_64 RELEASE=${RELEASE} LOGGING=${LOGGING}
+}
+
+build_arm() {
+    echo -e "Building for arm"
+    scons TARGET_ARCH=arm TC_PREFIX=/usr/bin/arm-linux-gnueabi- TC_PATH=/usr/bin/ RELEASE=${RELEASE} LOGGING=${LOGGING}
+}
+
+build_arm64() {
+    echo -e "Building for arm64"
+    scons TARGET_ARCH=arm64 TC_PREFIX=/usr/bin/aarch64-linux-gnu- TC_PATH=/usr/bin/ RELEASE=${RELEASE} LOGGING=${LOGGING}
+}
+
+build_armhf() {
+    echo -e "Building for armhf"
+    scons TARGET_ARCH=armhf TC_PREFIX=/usr/bin/arm-linux-gnueabihf- TC_PATH=/usr/bin/ RELEASE=${RELEASE} LOGGING=${LOGGING}
+}
+
+build_armhf_native() {
+    echo -e "Building for armhf_native"
+    scons TARGET_ARCH=armhf RELEASE=${RELEASE} LOGGING=${LOGGING}
+}
+
+build_armhf_qemu() {
+    echo -e "Building for armhf-qemu"
+    scons TARGET_ARCH=armhf RELEASE=${RELEASE} LOGGING=${LOGGING}
+
+    if [ -x "/usr/bin/qemu-arm-static" ]; then
+        echo -e "${BLUE}qemu-arm-static found, copying it to current directory${NO_COLOUR}"
+        cp /usr/bin/qemu-arm-static .
+    else
+        echo -e "${RED}No qemu-arm-static found${NO_COLOUR}"
+        echo -e "${BLUE} - Install qemu-arm-static and build again${NO_COLOUR}"
+    fi
+}
+
 build() {
     if [ true = ${AML_WITH_DEP} ]; then
         install_dependencies
     fi
-    cd $PROJECT_ROOT
 
-    RELEASE="1"
-    LOGGING="0"
     if [ "debug" = ${AML_BUILD_MODE} ]; then
         RELEASE="0"
     fi
@@ -95,7 +190,25 @@ build() {
         LOGGING="1"
     fi
 
-    scons TARGET_OS=linux TARGET_ARCH=${AML_TARGET_ARCH} RELEASE=${RELEASE} LOGGING=${LOGGING}
+    cd $PROJECT_ROOT
+    if [ "x86" = ${AML_TARGET_ARCH} ]; then
+         build_x86;
+    elif [ "x86_64" = ${AML_TARGET_ARCH} ]; then
+         build_x86_64;
+    elif [ "arm" = ${AML_TARGET_ARCH} ]; then
+         build_arm;
+    elif [ "arm64" = ${AML_TARGET_ARCH} ]; then
+         build_arm64;
+    elif [ "armhf" = ${AML_TARGET_ARCH} ]; then
+         build_armhf;
+    elif [ "armhf-qemu" = ${AML_TARGET_ARCH} ]; then
+         build_armhf_qemu;
+    elif [ "armhf-native" = ${AML_TARGET_ARCH} ]; then
+         build_armhf_native;
+    else
+         echo -e "${RED}Not a supported architecture${NO_COLOUR}"
+         usage; exit 1;
+    fi
     if [ $? -ne 0 ]; then 
         echo -e "${RED}Build failed${NO_COLOUR}" 
         exit 1 
